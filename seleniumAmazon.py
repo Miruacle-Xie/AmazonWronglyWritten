cdbc18a083c71c0cc38a335614edd76c019905ab
Miruacle-Xie <53250282+Miruacle-Xie@users.noreply.github.com>
2022-09-21 17:09:36 +0800

    Add files via upload

--- seleniumAmazon.py
+++ seleniumAmazon.py
@@ -188,7 +188,7 @@ def main():
     if not DEBUG:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"')
-    # chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--incognito')
     driver = webdriver.Chrome(chrome_options=chrome_options)
     # driver = webdriver.Chrome()
     if amazonDeliverInit(driver):
